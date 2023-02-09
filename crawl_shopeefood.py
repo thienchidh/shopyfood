8b0104bd13db0d7b14a97cefdcc8e68efb240207
chi <thienchidh@gmail.com>
2023-02-09 16:21:58 +0700

    init commit

---
+++ crawl_shopeefood.py
@@ -0,0 +1,129 @@
+import requests
+
+headers = {
+    'authority': 'gappapi.deliverynow.vn',
+    'accept': 'application/json, text/plain, */*',
+    'accept-language': 'vi,en;q=0.9,und;q=0.8,mt;q=0.7',
+    'dnt': '1',
+    'origin': 'https://shopeefood.vn',
+    'referer': 'https://shopeefood.vn/',
+    'sec-ch-ua': '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'cross-site',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    'x-foody-access-token': '',
+    'x-foody-api-version': '1',
+    'x-foody-app-type': '1004',
+    'x-foody-client-id': '',
+    'x-foody-client-language': 'vi',
+    'x-foody-client-type': '1',
+    'x-foody-client-version': '3.0.0'
+}
+
+
+def get_api_delivery(from_url):
+    url = 'https://gappapi.deliverynow.vn/api/delivery/get_from_url?url=' + from_url
+
+    print('get_api_delivery', url)
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Request failed with status code: ", response.status_code)
+    return None
+
+
+def get_api_detail(request_id):
+    url = 'https://gappapi.deliverynow.vn/api/delivery/get_detail?id_type=2&request_id=' + request_id
+    print('get_api_detail', url)
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Request failed with status code: ", response.status_code)
+    return None
+
+
+def get_api_delivery_dishes(request_id):
+    url = 'https://gappapi.deliverynow.vn/api/dish/get_delivery_dishes?id_type=2&request_id=' + request_id
+
+    print('get_api_delivery_dishes', url)
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Request failed with status code: ", response.status_code)
+    return None
+
+
+def get_url_with_no_params(url):
+    return url.split('?')[0]
+
+
+def get_url_with_no_base(url):
+    return url.replace('https://shopeefood.vn/', '')
+
+
+def is_support_url(url):
+    return url.startswith('https://shopeefood.vn/')
+
+
+def get_content_response(body):
+    if body is None:
+        return None
+
+    if body['result'] != 'success':
+        return None
+
+    reply = body['reply']
+    if reply is None:
+        return None
+
+    return reply
+
+
+def safe_get(dict, keys, default=None):
+    if dict is None:
+        return default
+
+    for key in keys:
+        if key not in dict:
+            return default
+        dict = dict[key]
+        if dict is None:
+            return default
+
+    return dict
+
+
+def process(full_url):
+    url = get_url_with_no_base(full_url)
+    url = get_url_with_no_params(url)
+
+    delivery_id = safe_get(get_content_response(get_api_delivery(url)), ['delivery_id'])
+    if delivery_id is None:
+        return None
+
+    title = safe_get(get_content_response(get_api_detail(str(delivery_id))), ['delivery_detail', 'name'])
+
+    dishes = get_content_response(get_api_delivery_dishes(str(delivery_id)))
+    menu_infos = safe_get(dishes, ['menu_infos'], [])
+
+    items = []
+    for menu_info in menu_infos:
+        for dish in menu_info['dishes']:
+            items.append({
+                'name': dish['name'],
+                'price': dish['price']['text'],
+            })
+
+    return title, items
---
+++ main.py
@@ -0,0 +1,147 @@
+import logging
+
+from telegram import (
+    ReplyKeyboardRemove,
+    Update,
+)
+from telegram.constants import ParseMode
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    PollAnswerHandler,
+    filters,
+)
+
+import crawl_shopeefood
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+
+async def poll(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    text = update.message.text
+    url = text.split(' ')[1]
+    obj = crawl_shopeefood.process(url)
+    title = obj[0]
+    items = obj[1]
+
+    questions = []
+    limit_question = 10
+
+    for item in items:
+        limit_question -= 1
+        if limit_question == 0:
+            break
+
+        questions.append(item['name'] + ' ' + item['price'])
+
+    if len(questions) < 2:
+        await update.message.reply_text('Không có đủ món ăn để tạo poll')
+        return
+
+    message = await context.bot.send_poll(
+        update.effective_chat.id,
+        title,
+        questions,
+        is_anonymous=False,
+        allows_multiple_answers=False,
+    )
+    # Save some info about the poll the bot_data for later use in receive_poll_answer
+    payload = {
+        message.poll.id: {
+            "questions": questions,
+            "message_id": message.message_id,
+            "chat_id": update.effective_chat.id,
+            "answers": 0,
+        }
+    }
+    context.bot_data.update(payload)
+    print("Poll created " + str(message.message_id) + " " + str(update.effective_chat.id))
+
+
+async def receive_poll_answer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Summarize a users poll vote"""
+    answer = update.poll_answer
+    answered_poll = context.bot_data[answer.poll_id]
+    try:
+        questions = answered_poll["questions"]
+    # this means this poll answer update is from an old poll, we can't do our answering then
+    except KeyError:
+        return
+    selected_options = answer.option_ids
+    answer_string = ""
+    for question_id in selected_options:
+        if question_id != selected_options[-1]:
+            answer_string += questions[question_id] + " and "
+        else:
+            answer_string += questions[question_id]
+
+    if answer_string == "":
+        return
+
+    await context.bot.send_message(
+        answered_poll["chat_id"],
+        f"{update.effective_user.mention_html()} chọn {answer_string}!",
+        parse_mode=ParseMode.HTML,
+    )
+    answered_poll["answers"] += 1
+    # Close poll after three participants voted
+    # if answered_poll["answers"] == 3:
+    #     await context.bot.stop_poll(answered_poll["chat_id"], answered_poll["message_id"])
+
+
+async def receive_poll(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """On receiving polls, reply to it by a closed poll copying the received poll"""
+    actual_poll = update.effective_message.poll
+    # Only need to set the question and options, since all other parameters don't matter for
+    # a closed poll
+    await update.effective_message.reply_poll(
+        question=actual_poll.question,
+        options=[o.text for o in actual_poll.options],
+        # with is_closed true, the poll/quiz is immediately closed
+        is_closed=True,
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+
+async def close_poll_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # Close the poll reply to the command
+    message = update.effective_message.reply_to_message
+    print("Close poll " + str(message.chat.id) + " " + str(message.message_id))
+    await context.bot.stop_poll(message.chat.id, message.message_id)
+
+    await message.reply_text("Poll closed by " + update.effective_user.first_name)
+    # Get results of the poll
+    poll = message.poll
+
+
+
+async def help_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Display a help message"""
+    await update.message.reply_text("Use /poll shopeefood_url to create a poll.")
+
+
+def main() -> None:
+    """Run bot."""
+    bot_token = "6173598005:AAE-ZNRagE3n6M3PHu4YRV690CC5gKrfoAs"
+
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token(bot_token).build()
+    application.add_handler(CommandHandler("start", help_handler))
+    application.add_handler(CommandHandler("poll", poll))
+    application.add_handler(CommandHandler("close", close_poll_handler))
+    application.add_handler(CommandHandler("help", help_handler))
+    application.add_handler(MessageHandler(filters.POLL, receive_poll))
+    application.add_handler(PollAnswerHandler(receive_poll_answer))
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
