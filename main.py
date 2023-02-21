01ac9ee5f0a8bc40f9409e8ac28a579438a645cc
thanhhv3 <thanhhv3@vng.com.vn>
2023-02-21 13:15:44 +0700

    feat: test new branch

--- main.py
+++ main.py
@@ -315,7 +315,7 @@ async def game_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 
 
 async def bill_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    print("Khai code")
+    print("Khai code 2")
     pass
 
 
