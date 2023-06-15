9ec022ec14934ca974ddb322d6642f489cdebaf4
chi <thienchidh@gmail.com>
2023-06-15 14:51:40 +0700

    fix: noti

--- main.py
+++ main.py
@@ -75,8 +75,6 @@ async def poll(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     title = obj[0]
     items = obj[1]
     alert_msg = obj[2]
-    if alert_msg is not None:
-        await update.message.reply_text(alert_msg)
 
     # Split items into 10 items per poll
     limit_items_per_poll = 10
@@ -176,6 +174,9 @@ async def poll(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     random_fn = random.choice([send_random_quote, quiz_handler, quiz_handler_2])
     await random_fn(update, context)
+
+    if alert_msg is not None:
+        await update.message.reply_text(alert_msg)
 
 
 async def send_random_quote(update, context):
